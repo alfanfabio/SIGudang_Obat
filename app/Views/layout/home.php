7e38a3339adb8254e851e8fc7803b761e085413a
Alfan Fabio <s21910113@student.unklab.ac.id>
2022-11-06 01:41:23 +0800

    UI diperbaiki

--- app/Views/layout/home.php
+++ app/Views/layout/home.php
@@ -273,14 +273,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url(); ?>/vendor/jquery/jquery.min.js"></script>
-    <script src="<?= base_url(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://alfan.ananda-aditya.my.id/SIGudangObat/public/vendor/jquery/jquery.min.js"></script>
+    <script src="https://alfan.ananda-aditya.my.id/SIGudangObat/public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= base_url(); ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="https://alfan.ananda-aditya.my.id/SIGudangObat/public/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?= base_url(); ?>/js/sb-admin-2.min.js"></script>
+    <script src="https://alfan.ananda-aditya.my.id/SIGudangObat/public/js/sb-admin-2.min.js"></script>
 
 </body>
 
